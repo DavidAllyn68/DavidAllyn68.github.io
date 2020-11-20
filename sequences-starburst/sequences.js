a6f703e6044b5af8c1b7e61c5634b20cc5028526
DavidAllyn68 <DavidAllyn68@Fastmail.net>
2020-11-20 14:40:22 -0800

    formating changes

--- sequences-starburst/sequences.js
+++ sequences-starburst/sequences.js
@@ -5,7 +5,7 @@ var radius = Math.min(width, height) / 2;
 
 // Breadcrumb dimensions: width, height, spacing, width of tip/tail.
 var b = {
-  w: 30, h: 30, s: 10, t: 30
+  w: 100, h: 30, s: 10, t: 30
 };
 
 // Mapping of step names to colors.
