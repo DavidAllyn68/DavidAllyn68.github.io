5f88ca29695f8878026af84e3924c061f732493d
DavidAllyn68 <DavidAllyn68@Fastmail.net>
2020-11-24 14:58:47 -0800

    Update clock.js
    playing with colors

--- clock.js
+++ clock.js
@@ -11,7 +11,7 @@ var formatSecond = d3.time.format("%-S seconds"),
     formatMonth = d3.time.format("%B");
 
 var bar_color = d3.scale.linear()
-    .range(["hsl(200,0%,0%)", "hsl(0,60%,50%)"])
+    .range(["hsl(217, 45%, 0%)", "hsl(210, 77%, 56%)"])
     .interpolate(function(a, b) { var i = d3.interpolateString(a, b); return function(t) { return d3.hsl(i(t)); }; });
 
 var text_color = d3.scale.linear()
